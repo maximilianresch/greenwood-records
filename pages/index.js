f59ef64f10e7dcddbbafc920fc6f89c554df85a9
Maximilian Resch <maximilianresch119@gmail.com>
2020-08-12 11:29:19 +0200

    commit

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function App() {
     <Head>
     <title>greenwoodrecords</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="My page title" key="title" />
     </Head>
       <Home />
       <Navigation />
