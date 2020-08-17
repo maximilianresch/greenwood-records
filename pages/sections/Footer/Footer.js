a519aaaa53345977edbe11c190a963742384c2aa
Maximilian Resch <maximilianresch119@gmail.com>
2020-08-17 15:33:41 +0200

    commit

--- pages/sections/Footer/Footer.js
+++ pages/sections/Footer/Footer.js
@@ -1,34 +1,39 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import YouTubeIcon from "@material-ui/icons/YouTube";
 
 export default function Footer() {
   return (
-      
-    
-      <div className={styles.footer}>
-        <div>
-          <span className={styles.span}>
-            <p>-copyright- greenwoodrecords, record now</p>
-            <a>Impressum</a>
-            <a>AGB</a>
-            <a>Datenschutzerklärung</a>
-          </span>
-        </div>
-        <div>
-          <span className={styles.span}>
-            <div>Gföhlerstraße xx</div>
-            <div> 3552 Lengenfeld</div>
-            <div>Österreich</div>
-          </span>
-        </div>
-        <div>
-          <span className={styles.span}>
-            <div> f.hainzl@gwr.at</div>
-            <div> +43 1234 567890</div>
-            <div> -social media icons-</div>
-          </span>
-        </div>
+    <div className={styles.footer}>
+      <div>
+        <span className={styles.span}>
+          <p>-copyright- greenwoodrecords, record now</p>
+          <a>Impressum</a>
+          <a>AGB</a>
+          <a>Datenschutzerklärung</a>
+        </span>
       </div>
-    
+      <div>
+        <span className={styles.span}>
+          <div>Gföhlerstraße xx</div>
+          <div> 3552 Lengenfeld</div>
+          <div>Österreich</div>
+        </span>
+      </div>
+      <div>
+        <span className={styles.span}>
+          <div> f.hainzl@gwr.at</div>
+          <div> +43 699 17007298 </div>
+          <div>
+            
+            <FacebookIcon />
+            <InstagramIcon />
+            <YouTubeIcon />
+          </div>
+        </span>
+      </div>
+    </div>
   );
 }
