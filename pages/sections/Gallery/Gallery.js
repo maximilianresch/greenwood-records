265ca10164b5357d8f75727055610926aefc50fe
maximilianresch <maximilianresch119@gmail.com>
2021-02-11 13:19:53 +0100

    commit

--- pages/sections/Gallery/Gallery.js
+++ pages/sections/Gallery/Gallery.js
@@ -22,8 +22,6 @@ const useStyles = makeStyles((theme) => ({
   },
   gridList: {
     flexWrap: 'nowrap',
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: 'translateZ(0)',
   },
 }));
 const tileData = [
